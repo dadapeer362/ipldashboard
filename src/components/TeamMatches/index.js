d1db47aeac23abaa9a6656440510e016866b4aeb
Shaik Dadapeer <73059382+dadapeer362@users.noreply.github.com>
2022-09-27 15:41:10 +0530

    removed testid

--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -91,7 +91,7 @@ class TeamMatches extends Component {
   }
 
   getLoaderOval = () => (
-    <div className="loader-style" testid="loader">
+    <div className="loader-style">
       <Loader type="Oval" color="#ffffff" height={80} width={80} />
     </div>
   )
